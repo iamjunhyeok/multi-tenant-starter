5088563ed91af59d2839c0610f7596433d868665
iamjunhyeok <jeonjhyeok@gmail.com>
2026-04-15 11:14:58 +0900

    JSqlParser를 compileOnly로 변경

--- src/main/java/io/github/iamjunhyeok/multitenant/config/TenantMyBatisAutoConfig.java
+++ src/main/java/io/github/iamjunhyeok/multitenant/config/TenantMyBatisAutoConfig.java
@@ -1,14 +1,13 @@
 package io.github.iamjunhyeok.multitenant.config;
 
 import io.github.iamjunhyeok.multitenant.mybatis.TenantMyBatisInterceptor;
-import org.apache.ibatis.plugin.Interceptor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConditionalOnClass(Interceptor.class)
+@ConditionalOnClass(name = {"org.apache.ibatis.plugin.Interceptor", "net.sf.jsqlparser.parser.CCJSqlParserUtil"})
 @ConditionalOnProperty(prefix = "tenant", name = "isolation", havingValue = "ROW", matchIfMissing = true)
 public class TenantMyBatisAutoConfig {
 
